91390155b7acf97696635bfc5d6204d07df8fa9c
Tomas-Mecius <tmecius@gmail.com>
2021-01-20 08:02:53 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 
 def generate_password():
@@ -25,20 +26,42 @@ def save():
     website = website_entry_field.get()
     username = username_password_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": username,
+        "password": password,
+    }}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {username} \nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", mode="a") as data_file:
-                data_file.write(f"{website} | {username} | {password}\n")
-                delete_data()
+        try:
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry_field.delete(0, END)
+            password_entry.delete(0, END)
 
 
-def delete_data():
-    website_entry_field.delete(0, END)
-    password_entry.delete(0, END)
+def find_password():
+    user_input = website_entry_field.get()
+    try:
+        with open("data.json") as data:
+            data = json.load(data)
+            if user_input in data:
+                website_entry_field.insert(0, user_input)
+                password_entry.insert(0, data[user_input]["password"])
+            else:
+                messagebox.showinfo(title="Error", message="No details for website exists!!")
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found!")
 
 
 window = Tk()
@@ -49,15 +72,18 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=1)
-
+# Labels
 website_label = Label(text="Website:")
 username_email_label = Label(text="Email/Username:")
 password_label = Label(text="Password:")
 
-website_entry_field = Entry(width=35)
+# Entries
+website_entry_field = Entry(width=21)
 username_password_entry = Entry(width=21)
 password_entry = Entry(width=35)
 
+# Buttons
+search_button = Button(text="Seach", width=14, command=find_password)
 generate_button = Button(text="Generate Password", command=generate_password)
 add_button = Button(text="Add", width=35, command=save)
 
@@ -65,12 +91,13 @@ website_label.grid(column=0, row=3)
 username_email_label.grid(column=0, row=4)
 password_label.grid(column=0, row=5)
 
-website_entry_field.grid(column=1, row=3, columnspan=2)
+website_entry_field.grid(column=1, row=3)
 website_entry_field.focus()
 username_password_entry.grid(column=1, row=4)
 username_password_entry.insert(0,"tmecius@gmail.com")
 password_entry.grid(column=1, row=5, columnspan=2)
 
+search_button.grid(column=2, row=3)
 generate_button.grid(column=2, row=4)
 add_button.grid(column=1, row=6, columnspan=2)
 
